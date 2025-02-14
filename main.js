67172f44e05c8520255ba7c19bcc9d685ae014c1
uthaya-uitoux <uthaya@uitoux.in>
2025-02-14 13:53:27 +0530

    add the files

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js')
     }
   })
-  win.loadFile("index.html")
+  win.loadFile("header.html")
  //win.webContents.openDevTools()
 }
 
